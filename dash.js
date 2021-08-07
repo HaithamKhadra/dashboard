b158189c7f9141e39186a2deade518c381270c4d
haithamkhadra <haithamkhadra@hotmail.com>
2021-08-07 22:16:38 +0300

    changed the darkmode toggle from radio input to checkbox input

--- dash.js
+++ dash.js
@@ -1,35 +1,9 @@
-// document.addEventListener('DOMContentLoaded', () => {
+const darkModeToggle = document.querySelector('#checkbox');
 
-  
-// });
-
-
-// const darkModeToggle = document.querySelector('#dark');
- 
-// darkModeToggle.addEventListener('click', () => {
-//   if (darkModeToggle.checked === true) {
-//     document.body.classList.add('dark');
-//   } else {  
-//     document.body.classList.remove('dark');
-//   }
-// });
-// const chk = document.querySelector('#default');
-
-// chk.addEventListener('click', () => {
-//   if (chk.checked == true && chk.id === 'default') {
-//     document.body.classList.add('dark');
-//   }
-// });
-
-const darkModeToggle = document.querySelectorAll('#dark, #light');
-
-darkModeToggle.forEach(inp => {
-  addEventListener('click', () => {
-    if (inp.checked == true && inp.id === 'dark') {
-      document.body.classList.add('dark');
-    } else {  
-      document.body.classList.remove('dark');
-    }
-  });
-})
-
+addEventListener('click', () => {
+  if (darkModeToggle.checked === true) {
+    document.body.classList.add('dark');
+  } else {  
+    document.body.classList.remove('dark');
+  }
+});
